c59811b28a04913c90fb54a698aff9913576a912
rummansheikh69 <rummansheikh2004@gmail.com>
2025-05-19 23:19:10 +0600

    edit

--- src/components/Nft.jsx
+++ src/components/Nft.jsx
@@ -12,7 +12,7 @@ function Nft() {
         <img src="images/divider.svg" alt="" />
       </div>
       <h1 className=" text-center font-asthetic text-4xl mt-10 text-zinc-800">
-        NFT COLLECTTIONS
+        MEMECOIN
       </h1>
       <div className=" px-5 md:px-20 mt-20">
         <div className=" grid grid-cols-2 md:grid-cols-4 gap-10">
