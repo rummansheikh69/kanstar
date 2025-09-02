44e4e7b88b17ad0c26579ce52212ab87f38ee277
Rumman Sheikh <rummansheikh2004@gmail.com>
2025-09-02 23:21:04 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
     <div className=" w-full bg-[#A17DB3] mt-32 border-t-4 border-black">
       <div className=" px-10 md:px-40 py-5 text-white">
         <div className=" flex items-center justify-between">
-          <p>© {new Date().getFullYear()} $GLORBZ</p>
+          <p>© {new Date().getFullYear()} $HAB</p>
           <div className=" flex items-center gap-4">
             <a href="https://x.com/kanstarsolana" target="_blank">
               <FaTwitter className=" w-6 h-6" />
